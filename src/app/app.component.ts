84360902f1262c11d8fee156283c5230aee2cc9d
Shailender Singh <geekshailender@gmail.com>
2025-02-15 11:46:13 -0500

    Routing using Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'project';
+  isMenuOpen = false; // For responsive menu
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
